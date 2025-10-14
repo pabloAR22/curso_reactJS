069aa17b1cda67771029206f2e210dc070d3cd02
Pablo <radabeca23gmail.com>
2025-10-14 06:59:26 -0500

    Clase 6: formulario de nombre.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Counter from './components/Counter'
 import ToggleButton from './components/ToggleButton'
+import NameForm from './components/NameForm'
+
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <Counter></Counter>
-      <ToggleButton></ToggleButton>
+      <h1>Formulario de nombre</h1>
+      <NameForm></NameForm>      
     </>
   )
 }
